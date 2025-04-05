1e495410c0c35e0caec0f9b23fbce35035fc5c1d
geolyun <geolyun@gmail.com>
2025-04-05 20:13:18 +0900

    first SpringBoot Commit

--- src/main/java/com/example/aniting/model/RecommendationRequest.java
+++ src/main/java/com/example/aniting/model/RecommendationRequest.java
@@ -3,14 +3,14 @@ package com.example.aniting.model;
 import java.util.Map;
 
 public class RecommendationRequest {
-    private Map<String, String> response;
+    private Map<String, String> responses;
 
     public Map<String, String> getResponses() {
-        return response;
+        return responses;
     }
 
-    public void setResponses(Map<String, String> response) {
-        this.response = response;
+    public void setResponses(Map<String, String> responses) {
+        this.responses = responses;
     }
 
 }
