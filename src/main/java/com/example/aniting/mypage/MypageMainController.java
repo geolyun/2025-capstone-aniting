2db7cce26a7335488534d6363bd20d78693f80d7
kai97 <kai97@192.168.0.4>
2025-04-18 16:15:45 +0900

    feat : 회원정보수정 구현

--- src/main/java/com/example/aniting/mypage/MypageMainController.java
+++ src/main/java/com/example/aniting/mypage/MypageMainController.java
@@ -11,4 +11,14 @@ public class MypageMainController {
 		return "client/mypage/mypage";
 	}
 	
+	@GetMapping("/mypage/update")
+	public String update() {
+		return "client/user/update_user";
+	}
+	
+	@GetMapping("/mypage/delete")
+	public String delete() {
+		return "client/user/delete_user";
+	}
+	
 }
