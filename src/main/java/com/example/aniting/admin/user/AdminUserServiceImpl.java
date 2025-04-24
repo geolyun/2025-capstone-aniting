da0cdaa5854547dc9461a8486c25db1c6ac06c18
kai97 <kai97@192.168.0.29>
2025-04-25 00:45:01 +0900

    feat : 관리자 로그인 구현

---
+++ src/main/java/com/example/aniting/admin/user/AdminUserServiceImpl.java
@@ -0,0 +1,28 @@
+package com.example.aniting.admin.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.aniting.entity.Admin;
+import com.example.aniting.repository.AdminRepository;
+
+@Service
+public class AdminUserServiceImpl implements AdminUserService {
+
+	@Autowired
+	private AdminRepository adminRepository;
+	
+	@Override
+	public Admin validateAdmin(String adminId, String passwd) {
+		
+		Admin admin = adminRepository.findByAdminId(adminId);
+		
+	    if (admin != null && admin.getPasswd().equals(passwd)) {
+	        return admin;
+	    }
+	    
+	    return null;
+	    
+	}
+
+}
