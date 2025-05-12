a71552301aaaab7c0775fd249e4ce9049156b58d
kai97 <kai97@192.168.0.4>
2025-05-12 16:35:52 +0900

    feat : 대시보드 페이지 상세 데이터 수정

--- src/main/java/com/example/aniting/repository/RecommendResponseRepository.java
+++ src/main/java/com/example/aniting/repository/RecommendResponseRepository.java
@@ -2,11 +2,15 @@ package com.example.aniting.repository;
 
 import com.example.aniting.entity.RecommendResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecommendResponseRepository extends JpaRepository<RecommendResponse, Long> {
 
 	void deleteByUsersId(String usersId);
+	
+	@Query("SELECT COUNT(DISTINCT r.usersId) FROM RecommendResponse r")
+	long countDistinctUsersId();
 	
 }
